818e627fc61366c54eb8799a5a28b5e9a5d9bc8d
Jamal Ahmed <jamal.ahmed@student.moringaschool.com>
2024-08-25 23:50:40 +0300

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,48 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import Filters from './Filters';
+import PetBrowser from './PetBrowser';
+import data from './db.json';
 
-import Filters from "./Filters";
-import PetBrowser from "./PetBrowser";
 
-function App() {
+const App = () => {
+  const [filters, setFilters] = useState({ type: 'all' });
   const [pets, setPets] = useState([]);
-  const [filters, setFilters] = useState({ type: "all" });
+
+  // Callback to update the selected pet type
+  const handleChangeType = (type) => {
+    setFilters({ type });
+  };
+
+  // Callback to find pets based on the selected type
+  const handleFindPetsClick = () => {
+    let filteredPets = data.pets;
+
+    if (filters.type !== 'all') {
+      filteredPets = filteredPets.filter(pet => pet.type === filters.type);
+    }
+
+    setPets(filteredPets);
+  };
+
+  // Callback to adopt a pet
+  const handleAdoptPet = (id) => {
+    setPets(pets.map(pet =>
+      pet.id === id ? { ...pet, isAdopted: true } : pet
+    ));
+  };
 
   return (
-    <div className="ui container">
+    <div className='ui container'>
       <header>
-        <h1 className="ui dividing header">React Animal Shelter</h1>
+        <h1 className='ui dividing header'>React Animal Shelter</h1>
       </header>
-      <div className="ui container">
-        <div className="ui grid">
-          <div className="four wide column">
-            <Filters />
+      <div className='ui container'>
+        <div className='ui grid'>
+          <div className='four wide column'>
+            <Filters onChangeType={handleChangeType} onFindPetsClick={handleFindPetsClick} />
           </div>
-          <div className="twelve wide column">
-            <PetBrowser />
+          <div className='twelve wide column'>
+            <PetBrowser pets={pets} onAdoptPet={handleAdoptPet} />
           </div>
         </div>
       </div>
@@ -27,3 +51,6 @@ function App() {
 }
 
 export default App;
+
+
+
